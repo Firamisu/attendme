52438a57238243cf831b7cc5a5f487aa0b65f95c
eryksalle <eryk.dziobak@students.salle.url.edu>
2026-02-11 20:24:05 +0100

    views for student and teacher, some fixes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+import LoggedInLayout from "@/layouts/LoggedInLayout.vue";
+
+const route = useRoute();
+
+const layoutComponent = computed(() => {
+  switch (route.meta.layout) {
+    case "LoggedInLayout":
+      return LoggedInLayout;
+    default:
+      return "div";
+  }
+});
+</script>
 
 <template>
-  <RouterView />
+  <component :is="layoutComponent">
+    <RouterView />
+  </component>
 </template>
 
 <style scoped></style>
--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,16 +1,21 @@
-// import { ref } from "vue";
-// import { defineStore } from "pinia";
+import { defineStore } from "pinia";
+import type { User } from "@/backend/AttendMeBackendClientBase";
 
-// export const useAuthStore = defineStore("authStore", () => {
-//   const token = ref<string | null>(null);
-
-//   function setToken(newToken: string) {
-//     token.value = newToken;
-//   }
-
-//   function clearToken() {
-//     token.value = null;
-//   }
-
-//   return { setToken, clearToken, token };
-// });
+export const useAuthStore = defineStore("auth", {
+  state: () => ({
+    user: null as User | null,
+  }),
+  getters: {
+    isAuthenticated: (state) => state.user !== null,
+    isTeacher: (state) => state.user?.isTeacher === true,
+    isStudent: (state) => state.user?.isStudent === true,
+  },
+  actions: {
+    setUser(user: User) {
+      this.user = user;
+    },
+    clear() {
+      this.user = null;
+    },
+  },
+});
